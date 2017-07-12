857e6aef925680c4ffd94fbe193492fb82836b64
Кривошеїн Володимир Олександрович <V.Kryvoshein@corp.ukrtelecom.loc>
2017-07-12 12:03:12 +0300

    Set DEBUG=False for Heroku

--- vspk/settings.py
+++ vspk/settings.py
@@ -142,7 +142,9 @@ if os.getcwd() == '/app':
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
     # Allow all host headers.
-    ALLOWED_HOSTS = ['*']
+    ALLOWED_HOSTS = ['vspkdata.herokuapp.com']
+
+    DEBUG = False
 
     # Static asset configuration
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
