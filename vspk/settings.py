11f7c4f6054fabb477924c7f8f3b0780a9f5ae66
Кривошеїн Володимир Олександрович <V.Kryvoshein@corp.ukrtelecom.loc>
2017-07-14 13:52:52 +0300

    Change the settings.py

--- vspk/settings.py
+++ vspk/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '#$9ax-olm$!9_a)p8pau98!_i(b%le5^#d85rfh9qd9b#2e@si'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', 'vspk.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'vspk.pythonanywhere.com']
 
 
 # Application definition
